75e0cb458824a95bdde8862f53c56539d4a092ce
tb6147877 <tb6147877@hotmail.com>
2021-05-26 15:36:30 +0800

    modify texture format

--- CSC8502/Shaders/FinalFrag.glsl
+++ CSC8502/Shaders/FinalFrag.glsl
@@ -10,10 +10,10 @@ out vec4 fragColor;
 
 void main() {
 	vec3 color = texture(diffTex, Texcoord).rgb;
-	//vec3 result = vec3(1.0) - exp(-color * exposure);
+	vec3 result = vec3(1.0) - exp(-color * exposure);
 
 	const float gamma = 2.2;
-	//result = pow(result, vec3(1.0 / gamma));
-	//fragColor = vec4(result, 1.0);
-	fragColor = texture(diffTex, Texcoord);
+	result = pow(result, vec3(1.0 / gamma));
+	fragColor = vec4(result, 1.0);
+	//fragColor = texture(diffTex, Texcoord);
 }
--- CSC8502/nclgl/Assimp_Mesh.h
+++ CSC8502/nclgl/Assimp_Mesh.h
@@ -28,7 +28,7 @@ struct Texture {
 	std::string type;
 	std::string path;
 
-    static unsigned int TextureFromFile(const char* path, const std::string& directory, const std::string& type) {
+    static unsigned int TextureFromFile(const char* path, const std::string& directory, const std::string& t_type) {
         std::string filename = std::string(path);
         filename = directory + '/' + filename;
 
@@ -39,27 +39,22 @@ struct Texture {
         unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
         if (data)
         {
-            GLenum format;
-            if (nrComponents == 1)
+            GLenum format, type;
+            if (nrComponents == 1) {
                 format = GL_RED;
+                type= GL_RED;
+            }
             else if (nrComponents == 3) {
-                format = GL_RGB;
-                /*if ("texture_diffuse"==type)
-                {
-                    format = GL_SRGB;
-                }*/
+                format = "texture_diffuse" == t_type? GL_SRGB:GL_RGB;
+                type = GL_RGB;
             }
             else if (nrComponents == 4) {
-                format = GL_RGBA;
-                /*if ("texture_diffuse" == type)
-                {
-                    format = GL_SRGB_ALPHA;
-                }*/
+                format = "texture_diffuse" == t_type ? GL_SRGB_ALPHA : GL_RGBA;
+                type = GL_RGBA;
             }
                 
-
             glBindTexture(GL_TEXTURE_2D, textureID);
-            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, type, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
 
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
