f705547629d220de9d7cb172a21b2d1b6d22ee06
Wang Dawei <tb6147877@hotmail.com>
2021-12-27 01:34:55 +0800

    初步完成nft生成全部流程

--- CSC8502/Blank Project/TestNFT.cpp
+++ CSC8502/Blank Project/TestNFT.cpp
@@ -2,10 +2,9 @@
 #include "../nclgl/Shader.h"
 #include "../nclgl/Quad.h"
 
+#include <format>
+
 TestNFT::TestNFT(const int width, const int height, const std::string& vertPath, const std::string& fragPath) :NFT_Excutor(width, height, vertPath,fragPath){
-	m_bg = LoadTextureFromFile(m_bg_files[0].c_str());
-	m_ele1 = LoadTextureFromFile(m_ele1_files[0].c_str());
-	m_ele2 = LoadTextureFromFile(m_ele2_files[0].c_str());
 }
 
 
@@ -15,53 +14,56 @@ TestNFT::~TestNFT() {
 
 
 void TestNFT::GenerateNFTs() {
-	/*if (m_isCompleted)
+	if (m_isCompleted)
 	{
 		return;
 	}
 
+	std::string path{"C:/Users/tb614/Desktop/tests/"};
+	int counter = 0;
+
 	for (int i = 0; i < m_bg_files.size(); i++)
 	{
+		m_bg = LoadTextureFromFile(m_bg_files[i].c_str());
 		for (int j = 0; j < m_ele1_files.size(); j++)
 		{
-			for (size_t i = 0; i < m_ele2_files.size(); i++)
+			m_ele1 = LoadTextureFromFile(m_ele1_files[j].c_str());
+			for (int k = 0; k < m_ele2_files.size(); k++)
 			{
+				m_ele2 = LoadTextureFromFile(m_ele2_files[k].c_str());
+
 				glClear(GL_COLOR_BUFFER_BIT);
 
+				glDisable(GL_DEPTH_TEST);
+				glEnable(GL_BLEND);
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+				glUseProgram(m_nft_shader->GetProgram());
+
+
+				glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, m_bg);
+				m_quad->Draw();
+
+				glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, m_ele1);
+				m_quad->Draw();
+
+				glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
+				glActiveTexture(GL_TEXTURE0);
+				glBindTexture(GL_TEXTURE_2D, m_ele2);
+				m_quad->Draw();
+
+				std::string temp = path;
+				temp.append(std::to_string(counter)).append("_r.png");
+
+				SerializeTexture(temp.c_str());
+				counter++;
 			}
 		}
 	}
 
-	m_isCompleted = true;*/
-
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	glDisable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	//glBlendFunc(GL_ONE, GL_ONE);
-
-	glUseProgram(m_nft_shader->GetProgram());
-
-	
-	glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, m_bg);
-	m_quad->Draw();
-
-	glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, m_ele1);
-	m_quad->Draw();
-
-	glUniform1i(glGetUniformLocation(m_nft_shader->GetProgram(), "diffTex0"), 0);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, m_ele2);
-	m_quad->Draw();
-
-	if (!m_isCompleted)
-	{
-		SerializeTexture("C:/Users/tb614/Desktop/rrr.png");
-		m_isCompleted = true;
-	}
+	m_isCompleted = true;
 }
